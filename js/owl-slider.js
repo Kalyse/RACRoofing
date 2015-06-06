cd8c09f783f1a1f0c754e700e1357800a1beaf09
Chris Acky <chrisacky@gmail.com>
2015-06-06 18:26:21 +0100

    Commit of site

---
+++ js/owl-slider.js
@@ -0,0 +1,26 @@
+	// JavaScript Document
+
+$(document).ready(function(){
+	$("#owl-example").owlCarousel({
+			 		loop:true,
+			 		autoplay:true,
+			 		autoplayHoverPause:true,
+			 		autoPlay: 9000, //Set AutoPlay to 3 seconds
+					items : 4,
+					itemsDesktop : [1199,3],
+					itemsDesktopSmall : [979,3]
+				});
+	$('#slider-with-blocks-1').owlCarousel({        
+         
+         navigation : true, // Show next and prev buttons
+         slideSpeed : 300,
+         singleItem:true,
+         autoPlay: 12000,
+         navigationText: [
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>"],
+         transitionStyle:"goDown"
+        });
+});
+    
+
